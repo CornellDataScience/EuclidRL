eae07ea08a1963d986532376d0ef72d9a7ced4ed
rjf275 <rjf276@cornell.edu>
2025-12-09 18:03:08 -0500

    Remove SupervisedDataCollator from exports

--- prover/data/__init__.py
+++ prover/data/__init__.py
@@ -1,3 +1,3 @@
-from .datasets import load_jsonl_dataset, SupervisedDataCollator
+from .datasets import load_jsonl_dataset
 
-__all__ = ["load_jsonl_dataset", "SupervisedDataCollator"]
+__all__ = ["load_jsonl_dataset"]
